ab5b07e27d1d14d091d219e64b17812c6edda72c
PepoStojan <stojan.peposki@gmail.com>
2019-05-27 14:06:49 +0200

    postaven

---
+++ index.js
@@ -0,0 +1,74 @@
+
+
+// Beef
+
+document.querySelector(' .beef-add').addEventListener('click' , function name(params) {
+  document.querySelector('.beef1').style.display='block'
+} )
+
+document.querySelector(' .beef-minus').addEventListener('click' , function name(params) {
+    document.querySelector('.beef1').style.display='none'
+  } )
+
+
+//   Chiken
+document.querySelector(' .chiken-plus').addEventListener('click' , function name(params) {
+document.querySelector('.chiken1').style.display='block'
+} )
+
+document.querySelector(' .chiken-minus').addEventListener('click' , function name(params) {
+document.querySelector('.chiken1').style.display='none'
+} )
+    
+
+    // Salad
+
+document.querySelector(' .salad-plus').addEventListener('click' , function name(params) {
+document.querySelector('.salad').style.display='block'
+} )
+
+document.querySelector('.salad-minus').addEventListener('click' , function name(params) {
+document.querySelector('.salad').style.display='none'
+} )
+
+// Ketchap
+
+
+
+
+
+document.querySelector(' .cheese-plus').addEventListener('click' , function name(params) {
+    document.querySelector('.cheese1').style.display='block'
+    } )
+    
+    document.querySelector(' .cheese-minus').addEventListener('click' , function name(params) {
+        document.querySelector('.cheese1').style.display='none'
+    } )
+
+
+    document.querySelector(' .bacon-plus').addEventListener('click' , function name(params) {
+        document.querySelector('.bacon1').style.display='block'
+        } )
+        
+        document.querySelector(' .bacon-minus').addEventListener('click' , function name(params) {
+            document.querySelector('.bacon1').style.display='none'
+        } )
+        
+
+
+        document.querySelector('.kecap-plus').addEventListener('click' , function name(params) {
+            document.querySelector('.tomatoe').style.display='block'
+            } )
+            
+            document.querySelector('.kecap-minus').addEventListener('click' , function name(params) {
+                document.querySelector('.tomatoe').style.display='none'
+            } )
+    
+
+
+
+
+
+
+  
+  
